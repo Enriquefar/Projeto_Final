2bc055801de8cf84ca7049685e38e3232d1639f8
Enriquefar <efar031207@gmail.com>
2025-04-09 00:21:40 -0300

    Exercicio 25

---
+++ Exercicio_25.py
@@ -0,0 +1,7 @@
+num1 = float(input('Digite um número: '))
+num2 = float(input('Digite o segundo número: '))
+num3 = float(input('Digite o seu terceiro número: '))
+num = [num1, num2, num3]
+max_num = num[-2:]
+
+print(f"Os dois maiores números são: {max_num}")
