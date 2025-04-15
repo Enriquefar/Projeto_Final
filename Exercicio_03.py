2dd6221df7fc42368ee72417fe4146345bbfd888
Computação Unifil <compuni@unifil.br>
2025-04-15 20:07:14 -0300

    exercicio_03

---
+++ Exercicio_03.py
@@ -0,0 +1,4 @@
+raio = float(input('Digite o valor do raio em cm: '))
+area = 3.1416 * (raio ** 2)
+perimetro = 2 * 3.1416 * raio
+print(f'O valor da área é de: {area} e do perímetro é de {perimetro}.')
