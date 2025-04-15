1a50a08788b3b1e56e5f0abadb87c00d120fcde5
Computação Unifil <compuni@unifil.br>
2025-04-15 19:55:39 -0300

    Exercício 01

---
+++ Exercicio_01.py
@@ -0,0 +1,5 @@
+base = float(input('Digite o tamanho da base: '))
+altura = float(input('Digite o tamanho da altura: '))
+area = base * altura
+perimetro = (base * 2) + (altura * 2)
+print(f'O valor da área é de: {area} e o perímetro é de: {perimetro}.')
