c15e537b2abe9f4147e67e6212c35e9f72bbe8c5
Computação Unifil <compuni@unifil.br>
2025-04-15 20:04:31 -0300

    exercicio_02

---
+++ Exercicio_02.py
@@ -0,0 +1,4 @@
+tamanho = float(input('Digite o tamanho do quadrado: '))
+area = tamanho * tamanho
+perimetro = tamanho * 4
+print(f'A área desse quadrado é de: {area} e o perimetro é de: {perimetro}.')
